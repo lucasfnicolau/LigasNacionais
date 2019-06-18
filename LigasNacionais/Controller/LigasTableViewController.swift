5ceaac1e34aef4577c84702750b51095677994c7
Lucas Fernandez Nicolau <dev.lucasfn@icloud.com>
2019-06-18 01:46:25 -0300

    Criação do UITableViewController inicial e separação dos arquivos do projeto em grupos

---
+++ LigasNacionais/Controller/LigasTableViewController.swift
@@ -0,0 +1,27 @@
+//
+//  LigasTableViewController.swift
+//  LigasNacionais
+//
+//  Created by Lucas Fernandez Nicolau on 18/06/19.
+//  Copyright © 2019 Lucas Fernandez Nicolau. All rights reserved.
+//
+
+import UIKit
+
+class LigasTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+}
